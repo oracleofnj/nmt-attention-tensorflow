b658088355128d3ead133244bc7b2c0044a40f33
oracleofnj <jared.samet@aya.yale.edu>
2018-01-02 13:35:23 -0500

    SGD

--- utils/unrolled_rnn.py
+++ utils/unrolled_rnn.py
@@ -318,7 +318,7 @@ def make_train_op(loss, learning_rate, max_norm):
             max_norm,
             name='clipped_gradients'
         )
-        optimizer = tf.train.SGDOptimizer(learning_rate)
+        optimizer = tf.train.SGD(learning_rate)
         train_op = optimizer.apply_gradients(
             zip(clipped_gradients, trainable_variables),
         )
